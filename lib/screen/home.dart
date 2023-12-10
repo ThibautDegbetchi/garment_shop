b095c0454534762f4aad0f6d4e2f00c253b753ac
thibautdegbetchi <thibautdegbetchi@gmail.com>
2023-12-10 09:58:22 +0100

    add of gridView to show my pictures

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:garment_shop/components/color.dart';
 import 'package:garment_shop/helper/mediaqueryhelper.dart';
+import 'package:staggered_grid_view_flutter/widgets/staggered_grid_view.dart';
+import 'package:staggered_grid_view_flutter/widgets/staggered_tile.dart';
 
 class HomePage extends StatefulWidget {
    HomePage({super.key,required this.title});
@@ -14,6 +16,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   List category=['All','Popular','Recent','Recommended'];
   int selectedIndex =0;
+  List<String> pictures=['assets/image1.png','assets/image2.png','assets/image3.png','assets/image4.png','assets/image1.png','assets/image2.png','assets/image3.png','assets/image4.png'];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,87 +31,108 @@ class _HomePageState extends State<HomePage> {
           child: Image.asset('assets/image1.png'),
         )],
       ),
-      body: Container(
-        margin: const EdgeInsets.only(top: 10),
-        child: Column(
-          children: <Widget>[
-            searchBar(context),
-            Stack(
-              children: [
-                Container(
-                  margin: EdgeInsets.only(top: 25),
-                  width: width(context, 4/3),
-                  height: height(context, 4),
-                  decoration: BoxDecoration(
-                    color: tdPinkColor,
-                    borderRadius: BorderRadius.circular(15),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Container(
+          margin: const EdgeInsets.only(top: 10),
+          child: Column(
+            children: <Widget>[
+              searchBar(context),
+              Stack(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 25),
+                    width: width(context, 4/3),
+                    height: height(context, 4),
+                    decoration: BoxDecoration(
+                      color: tdPinkColor,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          top: 25,
+                          left: width(context, 3),
+                            child: RichText(text: TextSpan(text: 'Big Sale',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25)),),
+                        ),
+                        Positioned(
+                          top: 75,
+                          left: width(context, 3),
+                          child: RichText(text: TextSpan(text: 'bla bla bla bla bla blabla bla bla v v \nvblabla blablablablablablabla',style: TextStyle(fontWeight: FontWeight.normal)),),
+                        ),
+
+                      ],
+                    ),
                   ),
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        top: 25,
-                        left: width(context, 3),
-                          child: RichText(text: TextSpan(text: 'Big Sale',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25)),),
-                      ),
-                      Positioned(
-                        top: 75,
-                        left: width(context, 3),
-                        child: RichText(text: TextSpan(text: 'bla bla bla bla bla blabla bla bla v v \nvblabla blablablablablablabla',style: TextStyle(fontWeight: FontWeight.normal)),),
-                      ),
+                  Positioned(
+                    top: 10,
+                    left: 2,
+                    child: Image.asset('assets/image2.png',width: width(context, 3),height: height(context, 4),),)
+                ],
+              ),
+             Container(
 
-                    ],
-                  ),
-                ),
-                Positioned(
-                  top: 10,
-                  left: 2,
-                  child: Image.asset('assets/image2.png',width: width(context, 3),height: height(context, 4),),)
-              ],
-            ),
-           Container(
+               margin: EdgeInsets.all(height(context, 50)),
+               height: height(context, 25),
+               width: width(context, 1.5),
+               child: ListView.separated(
+                   scrollDirection: Axis.horizontal,
+                   //padding: EdgeInsets.all(width(context, 70)),
+                   itemCount: category.length,
+                   itemBuilder: (context,index){
+                     final currentCategory=category[index];
 
-             margin: EdgeInsets.all(height(context, 50)),
-             height: height(context, 25),
-             width: width(context, 1.5),
-             child: ListView.separated(
-                 scrollDirection: Axis.horizontal,
-                 //padding: EdgeInsets.all(width(context, 70)),
-                 itemCount: category.length,
-                 itemBuilder: (context,index){
-                   final currentCategory=category[index];
+                     return InkWell(
+                       onTap: (){
+                         setState(() {
+                           selectedIndex = index;
+                         });
+                         print(currentCategory);
+                       },
+                       child: Container(
+                         padding: EdgeInsets.all(height(context, 150)),
+                         width: width(context, 10),
+                         decoration: BoxDecoration(
+                           color:selectedIndex==index?tdPinkColor:tdGreyColor,
+                           borderRadius: BorderRadius.circular(10),
+                         ),
+                         child: Text(currentCategory,
+                         textAlign: TextAlign.center,
+                         style: TextStyle(
+                           fontSize: 15,
+                         ),),
+                       )
+                     );
 
-                   return InkWell(
-                     onTap: (){
-                       setState(() {
-                         selectedIndex = index;
-                       });
-                       print(currentCategory);
-                     },
-                     child: Container(
-                       padding: EdgeInsets.all(height(context, 150)),
-                       width: width(context, 10),
-                       decoration: BoxDecoration(
-                         color:selectedIndex==index?tdPinkColor:tdGreyColor,
-                         borderRadius: BorderRadius.circular(10),
-                       ),
-                       child: Text(currentCategory,
-                       textAlign: TextAlign.center,
-                       style: TextStyle(
-                         fontSize: 15,
-                       ),),
-                     )
-                   );
-
-                 }, separatorBuilder: (BuildContext context, int index) { 
-                   return SizedBox(width: width(context, 60),);
-             },),
-           ),
-            GridView(gridDelegate: gridDelegate)
-          ],
+                   }, separatorBuilder: (BuildContext context, int index) {
+                     return SizedBox(width: width(context, 60),);
+               },),
+             ),
+              StaggeredGridView.countBuilder(
+                shrinkWrap: true,
+                primary: false,
+                mainAxisSpacing: 14,
+                crossAxisSpacing: 12,
+                crossAxisCount:3,
+                  itemCount: pictures.length,
+                  //gridDelegate: gridDelegate,
+                itemBuilder: (BuildContext context, int index) {
+                  return buildImageCard();
+                },
+                staggeredTileBuilder:(int index)=> StaggeredTile.count(1,1),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
+
+}
+Widget buildImageCard() {
+  return Card(
+
+  );
 }
 
 Widget searchBar(BuildContext context){
