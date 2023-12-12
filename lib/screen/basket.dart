10759178993c8344ebcb165a3bed18edea0f5186
thibautdegbetchi <thibautdegbetchi@gmail.com>
2023-12-12 11:53:16 +0100

    .

--- lib/screen/basket.dart
+++ lib/screen/basket.dart
@@ -20,6 +20,8 @@ class ShopingCart extends StatefulWidget {
 }
 
 class _ShopingCartState extends State<ShopingCart> {
+  bool isCheck=false;
+  int  currentIndex=0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,27 +30,132 @@ class _ShopingCartState extends State<ShopingCart> {
         title: Text('widget.title'),
         centerTitle: true,
       ),
-      body: Container(
-        width: double.maxFinite,
-        height: double.maxFinite,
-        child: Stack(
-          children: [
-            /*Expanded(
-              flex: 4,
-                child: SingleChildScrollView(
-                  child: Column(
-                    
-                  ),
-                )
-            )*/
-            ProduceCard(context,widget.url,widget.name,widget.price)
-          ],
-        ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 4,
+            child: ListView.builder(
+                itemCount: 3,
+                itemBuilder: (context,index){
+                  return Card(
+                      margin: EdgeInsets.all(height(context, 20)),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      color: tdWhiteColor,
+                      elevation: 1,
+                      child: Container(
+                        height: height(context, 5),
+                        width: width(context, 1.1),
+                        padding: EdgeInsets.all(height(context, 60)),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Container(
+                                  height: height(context, 5.1),
+                                  width: width(context, 10),
+                                  decoration: BoxDecoration(
+                                    //color: tdGreyColor,
+                                      borderRadius: BorderRadius.circular(10),
+                                      image: DecorationImage(
+                                        image: AssetImage(widget.url),
+                                        fit: BoxFit.fill,
+                                      )
+                                  ),
+                                ),
+                                SizedBox(width: width(context, 50),),
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(widget.name,style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold
+                                    ),),
+                                    Text("\$${widget.price}",style: TextStyle(
+                                        fontSize: 22,
+                                        fontWeight: FontWeight.bold
+                                    ),)
+                                  ],
+                                ),
+                              ],
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Checkbox(
+                                  onChanged: (value){
+                                    currentIndex=index;
+                                      setState(() {
+                                        if(currentIndex==index)
+                                          isCheck=value!;
+                                    });
+                                  },
+                                  value: isCheck,
+                                  checkColor: tdWhiteColor,
+                                  activeColor: tdPinkColor,
+                                ),
+                                Container(
+                                  height: height(context, 40),
+                                  width: width(context, 15),
+                                  decoration: BoxDecoration(
+                                    //color: tdBlackColor,
+                                      borderRadius: BorderRadius.circular(5)
+                                  ),
+                                  child: Row(
+                                    children: [
+                                      InkWell(
+                                        onTap: (){},
+                                        child: Container(
+                                          child: Icon(Icons.add,
+                                            size: width(context, 80),),
+                                        ),
+                                      ),
+                                      Text("number"),
+                                      InkWell(
+                                        onTap: (){},
+                                        child: Container(
+                                          child: Icon(Icons.remove,
+                                            size: width(context, 80),),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      )
+                  );
+                }
+
+            ),
+      ),
+          Expanded(
+            flex: 0,
+              child: Container(
+
+              )
+          )
+          ]
       )
     );
   }
+  bool setCheckState(int index,bool check){
+    setState(() {
+      if(currentIndex==index){
+        isCheck=!check;
+      }
+    });
+    return check;
+  }
 }
-Widget ProduceCard(BuildContext context,String url,String name,double price){
+
+Widget ProduceCard(BuildContext context,String url,String name,double price,bool isCheck){
   return Card(
     margin: EdgeInsets.all(height(context, 20)),
     shape: RoundedRectangleBorder(
@@ -92,13 +199,50 @@ Widget ProduceCard(BuildContext context,String url,String name,double price){
                     fontWeight: FontWeight.bold
                   ),)
                 ],
-              )
+              ),
             ],
           ),
-
-
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Checkbox(
+                value: isCheck,
+                onChanged: (onChanged){},
+                checkColor: tdWhiteColor,
+                activeColor: tdPinkColor,
+              ),
+              Container(
+                height: height(context, 40),
+                width: width(context, 15),
+                decoration: BoxDecoration(
+                    //color: tdBlackColor,
+                    borderRadius: BorderRadius.circular(5)
+                ),
+                child: Row(
+                  children: [
+                    InkWell(
+                      onTap: (){},
+                      child: Container(
+                        child: Icon(Icons.add,
+                        size: width(context, 80),),
+                      ),
+                    ),
+                    Text("number"),
+                    InkWell(
+                      onTap: (){},
+                      child: Container(
+                        child: Icon(Icons.remove,
+                          size: width(context, 80),),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          )
         ],
       ),
     )
   );
 }
+
